6c8e7bd0072b3d2f30a1fe40c1855177dd9f65c0
Joel Júnior <joel.fmjr@gmail.com>
2025-05-14 11:51:59 -0300

    feat: Change ModelViewset to ViewSet

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -10,23 +10,178 @@ from core.models import Category, Transaction, UploadedFile
 from core.tasks import process_file_task
 
 
-class UploadedFileViewSet(viewsets.ModelViewSet):
-    queryset = UploadedFile.objects.all()
-    serializer_class = UploadedFileSerializer
+class UploadedFileViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = UploadedFile.objects.all()
+        serializer = UploadedFileSerializer(queryset, many=True)
+        return Response(serializer.data)
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        process_file_task.delay(serializer.instance.pk)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    def retrieve(self, request, pk=None):
+        try:
+            uploaded_file = UploadedFile.objects.get(pk=pk)
+            serializer = UploadedFileSerializer(uploaded_file)
+            return Response(serializer.data)
+        except UploadedFile.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def create(self, request):
+        serializer = UploadedFileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            process_file_task.delay(serializer.instance.pk)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        try:
+            uploaded_file = UploadedFile.objects.get(pk=pk)
+            serializer = UploadedFileSerializer(
+                uploaded_file, data=request.data
+            )
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
+        except UploadedFile.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def partial_update(self, request, pk=None):
+        try:
+            uploaded_file = UploadedFile.objects.get(pk=pk)
+            serializer = UploadedFileSerializer(
+                uploaded_file, data=request.data, partial=True
+            )
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
+        except UploadedFile.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def destroy(self, request, pk=None):
+        try:
+            uploaded_file = UploadedFile.objects.get(pk=pk)
+            uploaded_file.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except UploadedFile.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
 
-class CategoryViewSet(viewsets.ModelViewSet):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+class CategoryViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Category.objects.all()
+        serializer = CategorySerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        try:
+            category = Category.objects.get(pk=pk)
+            serializer = CategorySerializer(category)
+            return Response(serializer.data)
+        except Category.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def create(self, request):
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        try:
+            category = Category.objects.get(pk=pk)
+            serializer = CategorySerializer(category, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
+        except Category.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def partial_update(self, request, pk=None):
+        try:
+            category = Category.objects.get(pk=pk)
+            serializer = CategorySerializer(
+                category, data=request.data, partial=True
+            )
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
+        except Category.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def destroy(self, request, pk=None):
+        try:
+            category = Category.objects.get(pk=pk)
+            category.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Category.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
 
-class TransactionViewSet(viewsets.ModelViewSet):
-    queryset = Transaction.objects.all()
-    serializer_class = TransactionSerializer
+class TransactionViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Transaction.objects.all()
+        serializer = TransactionSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        try:
+            transaction = Transaction.objects.get(pk=pk)
+            serializer = TransactionSerializer(transaction)
+            return Response(serializer.data)
+        except Transaction.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def create(self, request):
+        serializer = TransactionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        try:
+            transaction = Transaction.objects.get(pk=pk)
+            serializer = TransactionSerializer(transaction, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
+        except Transaction.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def partial_update(self, request, pk=None):
+        try:
+            transaction = Transaction.objects.get(pk=pk)
+            serializer = TransactionSerializer(
+                transaction, data=request.data, partial=True
+            )
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
+        except Transaction.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def destroy(self, request, pk=None):
+        try:
+            transaction = Transaction.objects.get(pk=pk)
+            transaction.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Transaction.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
