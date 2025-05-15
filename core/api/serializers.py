6573f7fb18f961a77106bcbab6f2244526208f1d
Joel Júnior <joel.fmjr@gmail.com>
2025-05-15 13:39:35 -0300

    feat: Add amount validation to be positive numbers

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -4,14 +4,15 @@ from core.models import Category, Transaction, UploadedFile
 
 
 class UploadedFileSerializer(serializers.ModelSerializer):
-
     def update(self, instance, validated_data):
         if self.partial is True:
             instance.name = validated_data.get('name', instance.name)
-            instance.file_type = validated_data.get('file_type', instance.file_type)
+            instance.file_type = validated_data.get(
+                'file_type', instance.file_type
+            )
             instance.save()
             return instance
-        
+
         if validated_data.get('file'):
             raise serializers.ValidationError(
                 {'file': 'O arquivo não pode ser atualizado'}
@@ -20,7 +21,7 @@ class UploadedFileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UploadedFile
-        fields = ['id','file', 'name', 'file_type']
+        fields = ['id', 'file', 'name', 'file_type']
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -30,6 +31,13 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class TransactionSerializer(serializers.ModelSerializer):
+    def validate_amount(self, value):
+        if value <= 0:
+            raise serializers.ValidationError(
+                {'amount': 'O valor deve ser positivo'}
+            )
+        return value
+
     class Meta:
         model = Transaction
         exclude = ['source_file']
